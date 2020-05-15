89cdd68dfc8236c8cd2c28c10e7ebda1ff8cee01
Kilian Stallinger <kp.stallinger@gmail.com>
2020-05-15 19:52:39 +0200

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -30,6 +30,22 @@ module.exports = {
     env_production: {
       NODE_ENV: 'production'
     }
+  },
+  {
+    name: 'SOLAR',
+    script: 'packages/solar-service/dist/main.js',
+
+    // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
   }
 ]
 };
